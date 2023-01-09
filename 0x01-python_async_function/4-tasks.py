5a6dd940605e994762aff10f66ed65a0ba2a51a6
Menor25 <ajirimenor@gmail.com>
2023-01-09 21:37:21 +0100

    asynchronous coroutine wait_n()

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Defines the asynchronous coroutine wait_n()."""
+import asyncio
+import queue
+from typing import List
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn wait_random n times with the specified max_delay.
+    Return list of delay values.
+    """
+    array = []
+    queue = []
+    for i in range(n):
+        task = task_wait_random(max_delay)
+        task.add_done_callback(lambda x: array.append(x.result()))
+        queue.append(task)
+
+    for task in queue:
+        await task
+
+    return array
